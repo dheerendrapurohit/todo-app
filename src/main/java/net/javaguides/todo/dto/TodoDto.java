0215158e1401e8d7c2535e9bb3e8d2abd5674650
dheerendrapurohit <dheerendra.cse.rymec@gmail.com>
2024-10-25 10:56:12 +0530

    Initial commit

---
+++ src/main/java/net/javaguides/todo/dto/TodoDto.java
@@ -0,0 +1,21 @@
+package net.javaguides.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDto {
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean completed;
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
